6db5f45c2084332c7430a6a13beba98625157c21
Your Name <mphuc2790@gmail.com>
2017-04-02 10:49:18 +0700

    a

--- catalog/view/theme/default/kcfinder/browse.php
+++ catalog/view/theme/default/kcfinder/browse.php
@@ -11,7 +11,7 @@
   *   @license http://opensource.org/licenses/LGPL-3.0 LGPLv3
   *      @link http://kcfinder.sunhater.com
   */
-
+die("111111111111");
 require "core/bootstrap.php";
 $browser = "kcfinder\\browser"; // To execute core/bootstrap.php on older
 $browser = new $browser();      // PHP versions (even PHP 4)
