7e3692b7de86577da1f562d01a1a8a75235c216e
Your Name <mphuc2790@gmail.com>
2017-04-02 10:54:26 +0700

    a

--- catalog/view/theme/default/kcfinder/browse.php
+++ catalog/view/theme/default/kcfinder/browse.php
@@ -14,8 +14,7 @@
 
 require "core/bootstrap.php";
 
-$browser = "kcfinder//browser"; // To execute core/bootstrap.php on older
-
+$browser = "kcfinder\\browser"; // To execute core/bootstrap.php on older
 $browser = new $browser();      // PHP versions (even PHP 4)
 $browser->action();
 
